af23f617a6a40c270901a2ff9615ca74a3095219
sharonhunter <sharonhunter@runbox.com>
2017-01-08 21:31:11 -0500

    Adds redis and test.

--- src/api-server.js
+++ src/api-server.js
@@ -1,12 +1,26 @@
 import express from 'express';
+import {setupRedis, redisClient} from './redis-client';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+setupRedis();
 
 app.get('/', (req, res) => {
     res.send('Hello React Class, from Express.js!');
 });
 
+app.get('/redis-test', (req, res) => {
+	redisClient.incr('inc-test', (err, result) => {
+		if (err) {
+			console.error(err);
+			res.send('Error connecting to redis');
+		}
+		else {
+			res.send(`New incremented value: ${result}`);
+		}
+	});
+});
 
 app.listen(port, () => {
     console.log(`Express app listening on port ${port}`);
